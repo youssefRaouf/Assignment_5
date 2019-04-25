4ce4ad92516119eb4277c8d62ea6ebe82d1aa9ad
youssef raouf <youssefraouf32@gmail.com>
2019-04-25 16:12:07 +0200

    done for real

--- Assignment5/src/eg/edu/alexu/csd/datastructure/stack/cs78/ExpressionEvaluator.java
+++ Assignment5/src/eg/edu/alexu/csd/datastructure/stack/cs78/ExpressionEvaluator.java
@@ -4,12 +4,15 @@ public class ExpressionEvaluator implements IExpressionEvaluator {
 
 	public String infixToPostfix(String expression) {
 		char[] postfix = new char[100];
-		String s = new String() ;
+		String s = new String();
 		int counter = 0;
 		stack stack = new stack();
-		stack stack1= new stack() ;
+		stack stack1 = new stack();
 		for (int i = 0; i < expression.length(); i++) {
 			char symb = expression.charAt(i);
+			if (symb == ' ') {
+				continue;
+			}
 			if (stack.size() == 0 && symb != '(' && symb != ')' && symb != '/' && symb != '*' && symb != '+'
 					&& symb != '-') {
 				stack.push(symb);
@@ -20,6 +23,7 @@ public class ExpressionEvaluator implements IExpressionEvaluator {
 			} else if ((symb == '+' || symb == '/' || symb == '*' || symb == '-') && stack1.peek() == (Object) '(') {
 				stack1.push(symb);
 			} else if (symb != '+' && symb != '/' && symb != '*' && symb != '-' && symb != '(' && symb != ')') {
+
 				stack.push(symb);
 			} else if (symb == '(') {
 				stack1.push(symb);
@@ -35,10 +39,21 @@ public class ExpressionEvaluator implements IExpressionEvaluator {
 					stack.pop();
 
 				}
-				while (stack1.size() != 0) {
+//	while (stack1.size() != 0) {
+				if (symb == ')') {
+					while (stack1.size() != 0) {
+						if (stack1.peek() == (Object) '(') {
+							stack1.pop();
+							break;
+						}
+						postfix[counter] = (char) stack1.peek();
+						counter++;
+						stack1.pop();
+					}
+				} else {
 					if (stack1.peek() == (Object) '(') {
 						stack1.pop();
-						break;
+
 					}
 					if (stack1.peek() != (Object) ')') {
 						postfix[counter] = (char) stack1.peek();
@@ -47,8 +62,15 @@ public class ExpressionEvaluator implements IExpressionEvaluator {
 					} else {
 						stack1.pop();
 					}
+//			}
 				}
-
+                 if(stack1.size()!=0&&(symb=='+'||symb=='-')&&(stack1.peek()==(Object)'/'||stack1.peek()==(Object)'*'||stack1.peek()==(Object)'+'||stack1.peek()==(Object)'-')) {
+                	while(stack1.size()!=0) {
+                		postfix[counter] = (char) stack1.peek();
+						counter++;
+						stack1.pop();
+                	}
+                 }
 				if (symb != ')') {
 					stack1.push(symb);
 				}
@@ -58,57 +80,68 @@ public class ExpressionEvaluator implements IExpressionEvaluator {
 		int j = 0;
 		int size = stack.size();
 		while (stack.size() != 0) {
-			postfix[size + temp - j - 1] = (char) stack.peek();
-			j++;
+			if (stack.peek() != (Object) '(' && stack.peek() != (Object) ')') {
+				postfix[size + temp - j - 1] = (char) stack.peek();
+				j++;
 //			 postfix[counter]=(char)stack.peek() ;
-			counter++;
+				counter++;
+			}
+//			j++;
 			stack.pop();
 		}
 		while (stack1.size() != 0) {
-			postfix[counter] = (char) stack1.peek();
-			counter++;
+			if (stack1.peek() != (Object) '(' && stack1.peek() != (Object) ')') {
+				postfix[counter] = (char) stack1.peek();
+				counter++;
+			}
 			stack1.pop();
 		}
 		for (int l = 0; l < counter; l++) {
-			s+=(char)postfix[l];
+			s += (char) postfix[l];
 		}
-		
 
-	return s ;
+		return s;
 	}
 
 	public int evaluate(String expression) {
-		stack value = new stack() ;
+		stack value = new stack();
+		int k = 0;
 		for (int i = 0; i < expression.length(); i++) {
 
-			if (expression.charAt(i) != '+' && expression.charAt(i) != '*' && expression.charAt(i) != '/' && expression.charAt(i) != '-') {
-				value.push((int) expression.charAt(i));
+			if (expression.charAt(i) != '+' && expression.charAt(i) != '*' && expression.charAt(i) != '/'
+					&& expression.charAt(i) != '-') {
+				value.push((double) expression.charAt(i));
 			} else {
-				int x = (int) value.pop() - 48;
-				int y = (int) value.pop() - 48;
+				double x = (double) value.pop() - 48;
+				double y = (double) value.pop() - 48;
 				if (expression.charAt(i) == '+') {
-					int z = (int) x + (int) y;
+					double z = (double) x + (double) y;
 					z += 48;
 					value.push(z);
 				}
 				if (expression.charAt(i) == '-') {
-					int z = (int) y - (int) x;
+					double z = (double) y - (double) x;
 					z += 48;
 					value.push(z);
 				}
 				if (expression.charAt(i) == '*') {
-					int z = (int) y * (int) x;
+					double z = (double) y * (double) x;
 					z += 48;
 					value.push(z);
 				}
 				if (expression.charAt(i) == '/') {
-					int z = (int) y / (int) x;
+					double z = (double) y / (double) x;
 					z += 48;
 					value.push(z);
 				}
+
+			}
+			if (i == expression.length() - 1) {
+				k = (int) ((double) value.pop() - 48);
 			}
 		}
-		return (int) value.pop() - 48 ;
+
+		return k;
 	}
 
 }
